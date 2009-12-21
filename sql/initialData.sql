6483339316e991f63deb3012a12856091027df3b
Jacobo Aragunde Perez <jaragunde@igalia.com>
2009-12-21 19:55:49 +0000

    Added a file with the basic user data to be inserted into the DB.
    Contains the minimum elements to be able to log into the application
    and start working with it.

---
+++ sql/initialData.sql
@@ -0,0 +1,27 @@
+--
+-- This is the minimum set of data for the application to work
+--
+
+
+--------------------------------------------------------------
+
+
+--
+-- Data for Name: user_group; Type: TABLE DATA; Schema: public; Owner: phpreport
+--
+
+INSERT INTO user_group VALUES (1, 'staff');
+
+
+--
+-- Data for Name: usr; Type: TABLE DATA; Schema: public; Owner: phpreport
+--
+
+INSERT INTO usr VALUES (1, '21232f297a57a5a743894a0e4a801fc3', 'admin');
+
+
+--
+-- Data for Name: belongs; Type: TABLE DATA; Schema: public; Owner: phpreport
+--
+
+INSERT INTO belongs VALUES (1, 1);
