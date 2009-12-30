2b3d0338c9431af91d5afb0d51f5a2555eda1234
Jorge Lopez Fernandez <jlopez@igalia.com>
2009-12-30 12:40:01 +0100

    Created a web service for retrieving the Users which are assigned to a Project.

---
+++ web/services/getProjectUsersService.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Copyright (C) 2009 Igalia, S.L. <info@igalia.com>
+ *
+ * This file is part of PhpReport.
+ *
+ * PhpReport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpReport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+   include_once('phpreport/web/services/WebServicesFunctions.php');
+   include_once('phpreport/model/facade/ProjectsFacade.php');
+   include_once('phpreport/model/vo/UserVO.php');
+
+   $sid = $_GET['sid'];
+
+   $pid = $_GET['pid'];
+
+    do {
+        /* We check authentication and authorization */
+        require_once('phpreport/util/LoginManager.php');
+
+        if (!LoginManager::isLogged($sid))
+        {
+            $string = "<users pid='$pid'><error id='2'>You must be logged in</error></users>";
+            break;
+        }
+
+        if (!LoginManager::isAllowed($sid))
+        {
+            $string = "<users pid='$pid'><error id='3'>Forbidden service for this User</error></users>";
+            break;
+        }
+
+        $users = ProjectsFacade::GetProjectUsers($pid);
+
+        $string = "<users pid='$pid'>";
+
+        foreach((array) $users as $retrievedUser)
+        {
+            $string = $string . "<user><id>{$retrievedUser->getId()}</id><login>{$retrievedUser->getLogin()}</login><userGroups>";
+
+            foreach ((array) $retrievedUser->getGroups() as $group)
+                $string = $string . "<{$group->getName()}>true</{$group->getName()}>";
+
+            $string = $string . "</userGroups></user>";
+        }
+
+        $string = $string . "</users>";
+
+    } while (False);
+
+   // make it into a proper XML document with header etc
+    $xml = simplexml_load_string($string);
+
+   // send an XML mime header
+    header("Content-type: text/xml");
+
+   // output correctly formatted XML
+    echo $xml->asXML();
