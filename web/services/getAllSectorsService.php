830540b398dbb58a3c1075bb9a8658f41d149d1d
Jorge Lopez Fernandez <jlopez@igalia.com>
2010-01-07 14:15:49 +0100

    Created a service for retrieving all Sectors.

---
+++ web/services/getAllSectorsService.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Copyright (C) 2009 Igalia, S.L. <info@igalia.com>
+ *
+ * This file is part of PhpReport.
+ *
+ * PhpReport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpReport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+   include_once('phpreport/web/services/WebServicesFunctions.php');
+   include_once('phpreport/model/facade/CustomersFacade.php');
+   include_once('phpreport/model/vo/SectorVO.php');
+
+    $sid = $_GET['sid'];
+
+    do {
+        /* We check authentication and authorization */
+        require_once('phpreport/util/LoginManager.php');
+
+        if (!LoginManager::isLogged($sid))
+        {
+            $string = "<sectors><error id='2'>You must be logged in</error></sectors>";
+            break;
+        }
+
+        if (!LoginManager::isAllowed($sid))
+        {
+            $string = "<sectors><error id='3'>Forbidden service for this User</error></sectors>";
+            break;
+        }
+
+        $sectors = CustomersFacade::GetAllSectors();
+
+        $string = "<sectors>";
+
+        foreach((array) $sectors as $sector)
+            $string = $string . "<sector><id>{$sector->getId()}</id><name>" . escape_string($sector->getName()) . "</name></sector>";
+
+        $string = $string . "</sectors>";
+
+    } while (False);
+
+   // make it into a proper XML document with header etc
+    $xml = simplexml_load_string($string);
+
+   // send an XML mime header
+    header("Content-type: text/xml");
+
+   // output correctly formatted XML
+    echo $xml->asXML();
