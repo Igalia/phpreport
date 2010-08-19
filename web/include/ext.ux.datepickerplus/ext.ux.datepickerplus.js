Igalia/phpreport

commit 8e3bde0ec311c952476c21cfd592a6c88ab74cb6

[Bug #101] Use ext.ux.datepickerplus instead of Datepickerlinks in tasks.php
This causes a regression on bug #30.

web/tasks.php
@@ -37,6 +37,7 @@ else
     $date = date("Y-m-d");
 
 ?>
+<script src="include/ext.ux.datepickerplus/ext.ux.datepickerplus.js"></script>
 <script type="text/javascript">
 
 function updateTimes(field, min, max, open) {
@@ -698,9 +699,10 @@ Ext.onReady(function(){
     new Ext.Panel({
         renderTo: Ext.get("auxiliarpanel"),
         items: [
-            new Ext.DatePickerLinks({
+            new Ext.ux.DatePickerPlus({
+                showWeekNumber: true,
+                multiselection: false,
                 value: Date.parseDate(date, 'Y-m-d'),
-                baseUrl: 'tasks.php?date=',
                 startDay: 1,
                 listeners: {'select': function (item, date) {
 			    window.location = "tasks.php?date=" + date.format('Y-m-d');

web/include/ext.ux.datepickerplus/datepickerplus.css
@@ -1,0 +1,166 @@
+﻿.x-date-right a.npy {
+    background-image: url(dbl-right-btn.gif) !important;
+}
+.x-date-left a.npy{
+    background-image: url(dbl-left-btn.gif) !important;
+}
+
+th.x-date-weeknumber-header a em span,
+.x-date-weeknumber-cell a em span{
+    color: #66f;
+    font-size:10px;
+    text-align:center;
+}
+
+th.x-date-weeknumber-header a:hover,
+.x-date-weeknumber-cell a:hover {
+    color: #66f !important;
+    background:none !important;
+    cursor:default !important;
+}
+
+.x-date-multiselect {
+    height:100%;
+}
+
+.x-date-multiselect th.x-date-weeknumber-header a:hover,
+.x-date-multiselect .x-date-weeknumber-cell a:hover {
+    background-color: #ddecfe !important;
+    cursor:pointer !important;
+}
+
+.x-date-monthtable {
+    border-left:1px solid #1b376c;
+}
+
+.x-date-pickerplus .x-date-dummy img {
+    height:1px;
+}
+
+
+
+/* Fix for Firefox 3 */
+.x-date-pickerplus .x-date-middle {
+    width: 120px;
+}
+
+.x-date-pickerplus .x-date-left a {
+    float:left !important;
+}
+
+.x-date-pickerplus .x-date-right a{
+    float:right !important;
+}
+
+.x-date-pickerplus .x-date-dummy,
+.x-date-pickerplus .x-date-dummy img,
+.x-date-pickerplus .x-date-left,
+.x-date-pickerplus .x-date-right {
+    width:40px !important;
+}
+
+.x-date-pickerplus  .x-date-middle {
+    font-weight:normal !important;
+    height:26px;
+}
+
+.x-date-multiokbtn,
+.x-date-multitodaybtn,
+.x-date-multiundobtn {
+    width:33%;
+}
+
+.x-datepickerplus-nationalholidays {
+    background-color:#ececec !important;
+}
+
+.x-datepickerplus-weekends {
+    background-color:#f4f4f4;
+}
+
+.x-datepickerplus-activedate a {
+    background-color: #ddecfe;
+    border:1px dashed #000088 !important;
+    padding:1px 4px;
+}
+
+.x-datepickerplus-eventdates a {
+    background-color:#ccccff;
+    border:1px solid #000088;
+    padding:1px 4px;
+}
+
+
+.x-date-inner .x-datepickerplus-eventdates-disabled a:hover {
+    background: #EB5F01;
+    color: #fff;
+}
+
+.x-date-inner .x-datepickerplus-eventdates-disabled a {
+    background: #EB5F01;
+    color: #ffffff;
+    cursor:default;
+}
+
+
+/*Set the em-tag to totally override the a-tag to prevent IE from opening a new Tab when CTRL-Clicking*/
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a em,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a em span {
+    height:19px !important;
+    cursor:pointer;
+}
+
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-eventdates a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-activedate a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-today a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-selected a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a:hover {
+    padding:0 !important;
+}
+
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a em {
+    width:100% !important;
+    padding:0 !important;
+}
+
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner a em span {
+    padding:2px 5px;
+}
+
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-eventdates a em span,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-activedate a em span,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-today a em span,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-selected a em span {
+    padding:1px 4px;
+}
+
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-eventdates a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-eventdates a em,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-eventdates a em span,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-activedate a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-activedate a em,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-datepickerplus-activedate a em span,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-today a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-today a em,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-today a em span,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-selected a,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-selected a em,
+.x-datepickerplus-ie .x-date-pickerplus .x-date-inner .x-date-selected a em span {
+    height:17px !important;
+}
+
+.x-date-pickerplus-prevnexthide .x-date-nextday,
+.x-date-pickerplus-prevnexthide .x-date-prevday {
+    visibility:hidden !important;
+}
+
+/*
+Reverse Ext 3.2.x Hack to display more than 1 months vertically on datemenu
+*/
+.ext-ie .x-date-menu,
+.ext-strict .ext-ie .x-date-menu, .ext-border-box .ext-ie8 .x-date-menu,
+.ext-strict .ext-ie7 .x-date-menu{
+    height: auto;
+}

web/include/phpreport.css
@@ -19,7 +19,7 @@ body {
 
 #content {
     float: both;
-    margin-left: 200px;
+    margin-left: 215px;
 }
 
 #footer {

commit d3d9486075754a722f9934fdbca18bf6c00293c3

[Bug #101] Fix regression introduced in 8e3bde0ec311c952476c21cfd592a6c88ab74cb6.
The modifications introduced in DatePickerLinks widget were moved to DatePickerPlus.

web/include/ext.ux.datepickerplus/ext.ux.datepickerplus.js
@@ -737,6 +737,14 @@ Ext.ux.DatePickerPlus = Ext.extend(Ext.DatePicker, {
 
     allowMouseWheel: true,
 
+    /**
+     * @cfg {Boolean/String} customLinkUrl
+     * Replaces the # link in days with a custom URL.
+     * Set to false to leave the default link behaviour.
+     * default is false
+     */
+    customLinkUrl: false,
+
 //this is accidently called too often in the original (when hovering over monthlabel or bottombar..there is no need to update the cells again and just leaks performance)
     focus: Ext.emptyFn,
 
@@ -1591,6 +1599,39 @@ Ext.ux.DatePickerPlus = Ext.extend(Ext.DatePicker, {
                 d.setDate(d.getDate()+1);
                 cells[i].className = "x-date-prevday";
                 setCellClass(this, cells[i],textEls[i],d);
+
+                if(this.customLinkUrl) {
+                    // We check the date for creating the right URL for href
+                    if (intDay < 10)
+                        dayString = "0" + intDay;
+                    else
+                        dayString = intDay;
+
+                    if (date.getMonth() < 9)
+                        monthString = "0" + (date.getMonth() + 1);
+                    else
+                        monthString = date.getMonth() + 1;
+                    dateString = date.getFullYear() + "-" + monthString + "-" + dayString;
+                    cells[i].firstChild.href = this.baseUrl + dateString;
+
+                    if (prevStart< 10)
+                        var dayString = "0" + prevStart;
+                    else
+                        var dayString = prevStart;
+
+                    if (date.getMonth() == 0) {
+                        var monthString = "12";
+                        var dateString = (date.getFullYear()-1) + "-" + monthString + "-" + dayString;
+                    }
+                    else {
+                        if (date.getMonth() < 10)
+                            var monthString = "0" + date.getMonth();
+                        else
+                            var monthString = date.getMonth();
+                        var dateString = date.getFullYear() + "-" + monthString + "-" + dayString;
+                    }
+                    cells[i].firstChild.href = this.customLinkUrl + dateString;
+                }
             }
 
             for(; i < days; ++i){
@@ -1605,6 +1646,21 @@ Ext.ux.DatePickerPlus = Ext.extend(Ext.DatePicker, {
                     this.todayMonthCell    = x;
                     this.todayDayCell = i;
                 }
+
+                if(this.customLinkUrl) {
+                    // We check the date for creating the right URL for href
+                    if (intDay < 10)
+                        dayString = "0" + intDay;
+                    else
+                        dayString = intDay;
+
+                    if (date.getMonth() < 9)
+                        monthString = "0" + (date.getMonth() + 1);
+                    else
+                        monthString = date.getMonth() + 1;
+                    dateString = date.getFullYear() + "-" + monthString + "-" + dayString;
+                    cells[i].firstChild.href = this.customLinkUrl + dateString;
+                }
             }
 
             var extraDays = 0;
@@ -1615,6 +1671,28 @@ Ext.ux.DatePickerPlus = Ext.extend(Ext.DatePicker, {
                 d.setDate(d.getDate()+1);
                 cells[i].className = "x-date-nextday";
                 setCellClass(this, cells[i],textEls[i],d);
+
+                if(this.customLinkUrl) {
+                    // We check the date for creating the right URL for href
+                    if (extraDays < 10)
+                        dayString = "0" + extraDays;
+                    else
+                        dayString = extraDays;
+
+                    if (date.getMonth() == 11) {
+                        monthString = "01";
+                        dateString = (date.getFullYear()+1) + "-" + monthString + "-" + dayString;
+                    }
+                    else {
+                        if (date.getMonth() < 8)
+                            monthString = "0" + (date.getMonth() + 2);
+                        else
+                            monthString = date.getMonth() + 2;
+                        dateString = date.getFullYear() + "-" + monthString + "-" + dayString;
+
+                    }
+                    cells[i].firstChild.href = this.customLinkUrl + dateString;
+                }
             }
 
             if (x===0 && !this.disableMonthPicker) {

web/tasks.php
@@ -703,6 +703,7 @@ Ext.onReady(function(){
                 allowMouseWheel: false,
                 showWeekNumber: true,
                 multiselection: false,
+                customLinkUrl: 'tasks.php?date=',
                 value: Date.parseDate(date, 'Y-m-d'),
                 startDay: 1,
                 listeners: {'select': function (item, date) {