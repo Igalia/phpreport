46969a43c8f49fc6408f2b2347841ad41406e02e
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2012-09-25 16:46:45 +0200

    Created columns in config table: block_tasks_by_time_enabled and block_tasks_by_time_number_of_days.

--- update/2.0-to-2.1.sql
+++ update/2.0-to-2.1.sql
@@ -30,3 +30,12 @@ create table config (
 
 INSERT INTO config(version) VALUES ('2.1');
 
+--
+-- Add new columns in config table:
+-- * block_tasks_by_time_enabled: enable/disable the block of tasks in the past.
+-- * block_tasks_by_time_number_of_days: how far in the past will tasks be blocked.
+--
+
+ALTER TABLE config ADD COLUMN block_tasks_by_time_enabled BOOLEAN
+    NOT NULL DEFAULT false;
+ALTER TABLE config ADD COLUMN block_tasks_by_time_number_of_days INTEGER;
