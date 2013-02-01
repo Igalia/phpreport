21bbac2c31e04dc71fcf709bea549bb305d1351d
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2013-02-01 09:36:42 +0100

    WIP

--- web/tasks.php
+++ web/tasks.php
@@ -208,6 +208,7 @@ var TaskPanel = Ext.extend(Ext.Panel, {
                 parent: this,
                 ref: '../initField',
                 allowBlank: false,
+                autoSelect: false,
                 width: 60,
                 format: 'H:i',
                 increment: 15,
@@ -218,6 +219,11 @@ var TaskPanel = Ext.extend(Ext.Panel, {
                 listeners: {
                     'change': function() {
                         this.parent.endTimeField.validate();
+                    },
+                    'keyup': function() {
+                        this.parent.endTimeField.validate();
+                    },
+                    'valid': function () {
                         this.parent.taskRecord.set('initTime',this.getValue());
                         updateTasksLength(this.parent);
                     }
@@ -231,6 +237,7 @@ var TaskPanel = Ext.extend(Ext.Panel, {
                 parent: this,
                 ref: '../endField',
                 allowBlank: false,
+                autoSelect: false,
                 width: 60,
                 format: 'H:i',
                 increment: 15,
@@ -241,6 +248,11 @@ var TaskPanel = Ext.extend(Ext.Panel, {
                 listeners: {
                     'change': function() {
                         this.parent.initTimeField.validate();
+                    },
+                    'keyup': function() {
+                        this.parent.initTimeField.validate();
+                    },
+                    'valid': function () {
                         this.parent.taskRecord.set('endTime',this.getValue());
                         updateTasksLength(this.parent);
                     }
@@ -395,14 +407,17 @@ var TaskPanel = Ext.extend(Ext.Panel, {
                     }
                 }
             }),
-            storyField: new Ext.form.Field({
+            storyField: new Ext.form.TextField({
                 parent: this,
                 value: this.taskRecord.data['story'],
                 tabIndex: tab++,
+                enableKeyEvents: true,
                 listeners: {
-                    'change': function() {
-                        this.setValue(Trim(this.getValue()));
+                    'keyup': function () {
                         this.parent.taskRecord.set('story',this.getValue());
+                    },
+                    'blur': function () {
+                        this.setValue(Trim(this.getValue()));
                     }
                 }
             }),
@@ -458,7 +473,8 @@ var TaskPanel = Ext.extend(Ext.Panel, {
                 enableKeyEvents: true,
                 listeners: {
                     'keyup': function () {
-                        this.parent.taskRecord.set('text',Trim(this.getValue()));
+                        this.parent.taskRecord.set('text',
+                                Trim(this.getValue()));
                     },
                     'blur': function () {
                         this.setValue(Trim(this.getValue()));
@@ -1091,6 +1107,7 @@ Ext.onReady(function(){
             }),
             '-',
             new Ext.Button({
+                id:'saveChangesButton',
                 text:'Save changes',
                 handler: saveTasks,
                 disabled: forbidden,
@@ -1103,7 +1120,21 @@ Ext.onReady(function(){
         key: 's',
         ctrl: true,
         stopEvent: true,
-        handler: saveTasks
+        handler: function () {
+            saveTasks();
+        }
+    });
+    new Ext.KeyMap(document, {
+        key: 'u',
+        ctrl: true,
+        stopEvent: true,
+        handler: newTask
+    });
+    new Ext.KeyMap(document, {
+        key: '123456789',
+        ctrl: true,
+        stopEvent: true,
+        handler: function () {alert("one")}
     });
 
     summaryStore.load();
