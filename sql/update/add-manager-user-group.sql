5781da59932343619e22db89913456becd3a0df6
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2016-11-21 14:00:36 +0100

    Add all admin users to manager group during upgrade
    User roles are incremental, so admin users are expected to have the
    manager role to work correctly. Automating this change will make the
    migration process less painful.
    
    Also, do not force the id '3' for manager user group to make the
    upgrade less error prone; it won't fail if that id has already been
    used.

--- sql/update/add-manager-user-group.sql
+++ sql/update/add-manager-user-group.sql
@@ -2,4 +2,13 @@
 -- Add new manager user group to the user_group table
 -- Restricts access to default staff profile
 --
-INSERT INTO user_group VALUES (3,'manager');
+INSERT INTO user_group(name) VALUES ('manager');
+
+--
+-- Add any admin user to the newly created manager group
+--
+INSERT INTO belongs(usrid, user_groupid)
+    SELECT usrid,
+        (SELECT id FROM user_group WHERE name='manager') AS user_groupid
+        FROM belongs
+        WHERE user_groupid=(SELECT id FROM user_group WHERE name='admin');
