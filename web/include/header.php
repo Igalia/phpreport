d30c0f18bd2d42f7cbfbd47dfc3d3710b51e2afe
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2018-02-09 14:30:15 +0100

    [#381] Configure favicon and shortcut icons for diverse platforms.
    Configuration and icons generated automatically with:
    https://realfavicongenerator.net/

--- web/include/header.php
+++ web/include/header.php
@@ -59,6 +59,15 @@
     <script src="include/ext.ux.datepickerplus/ext.ux.datepickerplus.js"></script>
     <script src="include/ext.ux.datepickerplus/ext.ux.datepickerplus-holidays.js"></script>
 
+    <!-- Favicon / shortcut icon configuration -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#dadada">
+    <meta name="theme-color" content="#dadada">
+
 </head>
 
 <body>
