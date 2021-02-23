a572270773ebf3784d3fa6094db43c7fcd2c4537
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2021-02-23 11:29:33 +0100

    Create a page to test the web services API.

---
+++ web/js/APITest.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 Igalia, S.L. <info@igalia.com>
+ *
+ * This file is part of PhpReport.
+ *
+ * PhpReport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpReport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function sendRequest() {
+  let url = document.getElementById('urlInput').value;
+  let request = document.getElementById('requestInput').value;
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      document.getElementById('responseInput').value = xhr.responseText;
+    }
+  }
+  xhr.send(request);
+}
+
+document.getElementById('sendButton').onclick = sendRequest;
