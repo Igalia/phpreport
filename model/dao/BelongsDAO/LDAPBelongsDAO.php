Igalia/phpreport

commit 981533e62f1e83dbd68cd80dc215385c37d86c90

[#431] Fix PHP warnings in accumulated hours report.
Fixes:
* 'undefined index' and 'undefined variable' errors in the
  web services layer
* 'undefined index' errors in the LDAP DAO implementation
* array access errors in the business logic layer

model/dao/BelongsDAO/LDAPBelongsDAO.php
@@ -75,13 +75,11 @@ class LDAPBelongsDAO extends BelongsDAO{
      */
     protected function setAValues($row)
     {
-
         $userVO = new UserVO();
 
-        $userVO->setId($row['id']);
         $userVO->setLogin($row['login']);
-        $userVO->setPassword($row['password']);
         $userVO->setGroups((array) $this->getByUserLogin($userVO->getLogin()));
+        // User data retrieved from LDAP does not contain ID or password
 
         return $userVO;
     }

model/facade/action/ExtraHoursReportAction.php
@@ -379,7 +379,7 @@ class ExtraHoursReportAction extends Action {
                 if ($previousInit != $auxDate)
                     $auxOtherExtraHours = $this->netExtraHours($previousInit, $auxDate, $user);
                 else
-                    $auxOtherExtraHours = 0;
+                    $auxOtherExtraHours = [$user->getLogin() => ["extra_hours" => 0]];
 
             $otherExtraHours[$user->getLogin()] = $auxOtherExtraHours[$user->getLogin()];
 

web/services/getExtraHoursReportService.php
@@ -31,19 +31,19 @@
     include_once(PHPREPORT_ROOT . '/model/facade/UsersFacade.php');
     include_once(PHPREPORT_ROOT . '/model/vo/UserVO.php');
 
-    $userLogin = $_GET['uid'];
+    $userLogin = $_GET['uid'] ?? "";
 
-    $init = $_GET['init'];
+    $init = $_GET['init'] ?? "";
 
-    $end = $_GET['end'];
+    $end = $_GET['end'] ?? "";
 
-    $dateFormat = $_GET['dateFormat'];
+    $dateFormat = $_GET['dateFormat'] ?? "Y-m-d";
 
-    $sid = $_GET['sid'];
+    $sid = $_GET['sid'] ?? NULL;
 
     $calculatePendingHolidays = false;
 
-    $csvExport = ($_GET["format"] && $_GET["format"] == "csv");
+    $csvExport = (isset($_GET["format"]) && $_GET["format"] == "csv");
     $csvFile = null;
     if($csvExport)
     {
@@ -95,9 +95,6 @@
             $string = $string . "><error id='3'>Forbidden service for this User</error></report>";
             break;
         }
-
-        if ($dateFormat=="")
-            $dateFormat = "Y-m-d";
 
         if ($init!="")
         {

web/services/getPendingHolidayHoursService.php
@@ -31,17 +31,17 @@
     include_once(PHPREPORT_ROOT . '/model/facade/UsersFacade.php');
     include_once(PHPREPORT_ROOT . '/model/vo/UserVO.php');
 
-    $userLogin = $_GET['uid'];
+    $userLogin = $_GET['uid'] ?? "";
 
-    $init = $_GET['init'];
+    $init = $_GET['init'] ?? "";
 
-    $end = $_GET['end'];
+    $end = $_GET['end'] ?? "";
 
-    $dateFormat = $_GET['dateFormat'];
+    $dateFormat = $_GET['dateFormat'] ?? "Y-m-d";
 
-    $sid = $_GET['sid'];
+    $sid = $_GET['sid'] ?? NULL;
 
-    $csvExport = ($_GET["format"] && $_GET["format"] == "csv");
+    $csvExport = (isset($_GET["format"]) && $_GET["format"] == "csv");
     $csvFile = null;
     if($csvExport)
     {
@@ -89,9 +89,6 @@
             break;
         }
 
-        if ($dateFormat=="")
-            $dateFormat = "Y-m-d";
-
         if ($init!="")
         {
             $initParse = date_parse_from_format($dateFormat, $init);
@@ -109,7 +106,7 @@
         } else
             $end = new DateTime();
 
-        if ($userId != "")
+        if ($userLogin != "")
         {
             $userVO = new UserVO();
             $userVO->setLogin($userLogin);

commit 2ce93600a20bc70b10641e563c1da32cbd93cb99

Fix regression in accumulated hours report with LDAP enabled.
This is a regression caused by commit
981533e62f1e83dbd68cd80dc215385c37d86c90.

The function LDAPBelongsDAO->setAValues may be called from
BaseRelationshipDAO->executeFromB, so it's not true that it never
receives the id and password fields.

Restored the original code in LDAPBelongsDAO->setAValues, and fixed
the PHP warning that we wanted to address in a different way.

model/dao/BelongsDAO/LDAPBelongsDAO.php
@@ -75,11 +75,13 @@ class LDAPBelongsDAO extends BelongsDAO{
      */
     protected function setAValues($row)
     {
+
         $userVO = new UserVO();
 
+        $userVO->setId($row['id']);
         $userVO->setLogin($row['login']);
+        $userVO->setPassword($row['password']);
         $userVO->setGroups((array) $this->getByUserLogin($userVO->getLogin()));
-        // User data retrieved from LDAP does not contain ID or password
 
         return $userVO;
     }
@@ -194,7 +196,9 @@ class LDAPBelongsDAO extends BelongsDAO{
 
         for ($i=0;$i<$info[0]["uniquemember"]["count"];$i++) {
             strtok($info[0]["uniquemember"][$i],"=,");
-            $user["login"]=strtok("=,");
+            $user["login"] = strtok("=,");
+            $user["id"] = null;
+            $user["password"] = null;
             $usersLDAP[]=$this->setAValues($user);
         }
 