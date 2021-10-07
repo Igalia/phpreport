183b0efa4b38b72b14d1aac9d7826e39b0b64e3b
Ana Rute Mendes <ana@igalia.com>
2021-10-07 12:31:22 -0300

    Return full holiday summary data
    Instead of returning just pending, return full summary.
    It will be used in the holiday management view, this change
    doesn't impact on other places that are expecting just
    pending hours.

--- web/services/getPersonalSummaryByDateService.php
+++ web/services/getPersonalSummaryByDateService.php
@@ -90,11 +90,26 @@ do {
 
     // Report holidays from the entire year, including those later than $date
     $endYearDate = new DateTime($date->format('Y') . '-12-31');
-    $holidays = UsersFacade::GetPendingHolidayHours($initYearDate, $endYearDate, $userVO);
-    $pendingHolidays = $holidays[$userVO->getLogin()];
+    $holidays = UsersFacade::GetHolidayHoursSummary($initYearDate, $endYearDate, $userVO);
+    $pendingHolidays = $holidays["pendingHours"][$userVO->getLogin()];
     $pendingHolidays = HolidayService::formatHours($pendingHolidays, $currentJourney, 5);
 
-    $string = "<personalSummary login='" . $userVO->getLogin() . "' date='" . $date->format($dateFormat) . "'><hours><day>" . $day  . "</day><week>" . $week  . "</week><weekly_goal>" . $weekGoal . "</weekly_goal><extra_hours>" . $extraHours . "</extra_hours><pending_holidays>" . $pendingHolidays . "</pending_holidays><acc_extra_hours>" . $accExtraHours . "</acc_extra_hours></hours></personalSummary>";
+    $scheduledHolidays = $holidays["scheduledHours"][$userVO->getLogin()];
+    $scheduledHolidays = HolidayService::formatHours($scheduledHolidays, $currentJourney, 5);
+
+    $availableHolidays = $holidays["availableHours"][$userVO->getLogin()];
+    $availableHolidays = HolidayService::formatHours($availableHolidays, $currentJourney, 5);
+
+    $string = "<personalSummary login='" . $userVO->getLogin() . "' date='" . $date->format($dateFormat) . "'>'
+                .'<hours><day>" . $day  . "</day>'
+                .'<week>" . $week  . "</week>'
+                .'<weekly_goal>" . $weekGoal . "</weekly_goal>'
+                .'<extra_hours>" . $extraHours . "</extra_hours>'
+                .'<pending_holidays>" . $pendingHolidays . "</pending_holidays>'
+                .'<scheduled_holidays>" . $scheduledHolidays . "</scheduled_holidays>'
+                .'<available_holidays>" . $availableHolidays . "</available_holidays>'
+                .'<acc_extra_hours>" . $accExtraHours . "</acc_extra_hours>'
+                .'</hours></personalSummary>";
 } while (false);
 
 // make it into a proper XML document with header etc
