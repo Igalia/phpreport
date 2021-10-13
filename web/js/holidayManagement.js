a758777451fca071a41b05cb1a72d667d3727f53
Ana Rute Mendes <ana@igalia.com>
2021-10-13 19:13:05 -0300

    Display today in the calendar
    It makes easier for the user to visualize enjoyed and scheduled holidays

--- web/js/holidayManagement.js
+++ web/js/holidayManagement.js
@@ -131,6 +131,12 @@ var app = new Vue({
                 dates: { start: new Date(dt.start + 'T00:00:00'), end: new Date(dt.end + 'T00:00:00') },
                 coveredDates: datesAndRanges.dates.filter(d => d >= dt.start && d <= dt.end)
             }));
+            // Add today
+            attributes.push({
+                bar: 'orange',
+                dates: { start: new Date(), end: new Date() },
+            });
+
             this.ranges = attributes;
             this.days = datesAndRanges.dates;
             this.total = datesAndRanges.dates.length;
