1c0e8b5f61c209ec0183398e078af36bbb2b9a4e
Ana Rute Mendes <ana@igalia.com>
2022-02-01 15:53:07 -0300

    Add DateOperations class

---
+++ util/DateOperations.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Copyright (C) 2022 Igalia, S.L. <info@igalia.com>
+ *
+ * This file is part of PhpReport.
+ *
+ * PhpReport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpReport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Phpreport\Util;
+
+class DateOperations
+{
+    public static function dateBelongsToPeriod(\DateTime $date, \DateTime $init, \DateTime $end): bool
+    {
+        return $date >= $init && $date <= $end;
+    }
+}
