35097582e00288343b949c64c456d616e5df5fed
Ana Rute Mendes <ana@igalia.com>
2022-06-09 18:53:50 +0200

    Add updated_at column to task table
    It will be used mainly to track when users update their vacation plans

---
+++ update/update-from-2.21-to-2.22.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Copyright (C) 2022 Igalia, S.L. <info@igalia.com>
+ *
+ * This file is part of PhpReport.
+ *
+ * PhpReport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpReport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('utils.php');
+
+define('PHPREPORT_ROOT', __DIR__ . '/../');
+define('SQLPATH', PHPREPORT_ROOT . 'sql/update/');
+
+/* These are the sql files that must be executed to prepare DB.
+ *
+ * IMPORTANT: they must be ordered for their proper execution.
+ */
+$sqlFiles = array();
+$sqlFiles[] = SQLPATH . "add-updated-at-to-task.sql";
+$sqlFiles[] = SQLPATH . "bump-db-version-2-22.sql";
+
+// run upgrade scripts
+
+require_once(PHPREPORT_ROOT . 'config/config.php');
+
+if (strcmp(get_db_version(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD), "2.21") != 0) {
+    print("Wrong database version. " .
+        "Make sure DB is on 2.21 version before running this upgrade.\n");
+    exit();
+}
+
+$success = true;
+foreach ($sqlFiles as $file) {
+    if (!parse_psql_dump($file, DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD)) {
+        $success = false;
+        break;
+    }
+}
+
+// finish, print message
+
+if ($success) {
+    print("Database update completed successfully\n");
+} else {
+    print("Error updating database in step: " . $file .
+        "\nPlease consider doing a manual update\n");
+}
