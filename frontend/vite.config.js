565856cd49dc256082798e4804cec358d8670a4f
Danielle Mayabb <danielle@igalia.com>
2023-07-26 11:54:41 -0700

    Rename outer folder for frontend
    Since there is an inner 'src' folder, renaming this will alleviate confusion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('/src', import.meta.url))
+      '@': fileURLToPath(new URL('/frontend', import.meta.url))
     }
   }
   // server: {
