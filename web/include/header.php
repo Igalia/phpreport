48261a1187e6694acd535cc7dbc883f7f51590d8
Danielle Mayabb <danielle@igalia.com>
2023-07-31 12:57:07 -0700

    Move images, svgs, favicons to assets folder
    Eventually all assets will live within `frontend`, but this cleans up the projects a little until that time.

--- web/include/header.php
+++ web/include/header.php
@@ -18,60 +18,63 @@
  * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-  header('Content-Type: text/html; charset=UTF-8');
+header('Content-Type: text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
+  <meta charset="UTF-8">
 
-    <title><?php echo PAGE_TITLE; ?></title>
+  <title>
+    <?php echo PAGE_TITLE; ?>
+  </title>
 
-    <!-- Include PhpReport stylesheet -->
-    <link rel="stylesheet" type="text/css" href="include/phpreport.css">
+  <!-- Include PhpReport stylesheet -->
+  <link rel="stylesheet" type="text/css" href="include/phpreport.css">
 
-    <!-- Include Ext stylesheet -->
-    <link rel="stylesheet" type="text/css" href="ext/resources/css/ext-all.min.css">
-    <link rel="stylesheet" type="text/css" href="include/silk.css" />
-    <link rel="stylesheet" type="text/css" href="include/tools.css" />
-    <link rel="stylesheet" type="text/css" href="ext/examples/ux/css/ux-all.css"/>
+  <!-- Include Ext stylesheet -->
+  <link rel="stylesheet" type="text/css" href="ext/resources/css/ext-all.min.css">
+  <link rel="stylesheet" type="text/css" href="include/silk.css" />
+  <link rel="stylesheet" type="text/css" href="include/tools.css" />
+  <link rel="stylesheet" type="text/css" href="ext/examples/ux/css/ux-all.css" />
 
-    <!-- Include DatePickerPlus stylesheet -->
-    <link rel="stylesheet" type="text/css" href="include/ext.ux.datepickerplus/datepickerplus.css"/>
+  <!-- Include DatePickerPlus stylesheet -->
+  <link rel="stylesheet" type="text/css" href="include/ext.ux.datepickerplus/datepickerplus.css" />
 
-    <!-- Include Ext library -->
-    <script src="ext/adapter/ext/base.js"></script>
-    <script src="ext/ext.js"></script>
-    <script src="include/App.js"></script>
+  <!-- Include Ext library -->
+  <script src="ext/adapter/ext/base.js"></script>
+  <script src="ext/ext.js"></script>
+  <script src="include/App.js"></script>
 
-    <!-- Configure prototypes -->
-    <script>
-      Ext.DatePicker.prototype.startDay = 1;
-    </script>
+  <!-- Configure prototypes -->
+  <script>
+    Ext.DatePicker.prototype.startDay = 1;
+  </script>
 
-    <!-- Include other common validations -->
-    <script src="include/validations.js"></script>
+  <!-- Include other common validations -->
+  <script src="include/validations.js"></script>
 
-    <!-- Include improved calendar widget -->
-    <script src="include/ext.ux.datepickerplus/ext.ux.datepickerplus.js"></script>
+  <!-- Include improved calendar widget -->
+  <script src="include/ext.ux.datepickerplus/ext.ux.datepickerplus.js"></script>
 
-    <!-- Periodical check for open sessions -->
-    <script src="js/include/sessionTracker.js"></script>
+  <!-- Periodical check for open sessions -->
+  <script src="js/include/sessionTracker.js"></script>
 
-    <!-- Favicon / shortcut icon configuration -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#dadada">
-    <meta name="theme-color" content="#dadada">
+  <!-- Favicon / shortcut icon configuration -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#dadada">
+  <meta name="theme-color" content="#dadada">
 
 </head>
 
 <body>
 
-    <?php include("include/menubar.php"); ?>
-    <script>
-        var HEADER_HEIGHT = document.getElementById('menubar').scrollHeight;
-    </script>
+  <?php include("include/menubar.php"); ?>
+  <script>
+    var HEADER_HEIGHT = document.getElementById('menubar').scrollHeight;
+  </script>
