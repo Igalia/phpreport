7402befc08107e117577373b9728eb459ba64a00
Danielle Mayabb <danielle@igalia.com>
2023-08-02 11:11:54 -0700

    Update auth handler, login manager, and env
    - Add more props to env: since OIDC providers may vary on what the property name is for username, it seems wise to make that a variable; add certs endpoint for token validation; separate client-id and audience, as those might vary, as well
    - Update auth handler to use certs endpoint to validate token
    - Clean up login manager

--- api/auth/auth_handler.py
+++ api/auth/auth_handler.py
@@ -1,11 +1,13 @@
 import time
 import jwt
+from jwt import PyJWKClient
 from decouple import config
 
 
 JWT_SECRET = config("JWT_SECRET")
 JWT_ALGORITHM = config("JWT_ALGORITHM")
 JWT_AUDIENCE = config("JWT_AUDIENCE")
+OIDC_CERTS_URL = config("OIDC_CERTS_URL")
 
 
 def token_response(token: str):
@@ -13,5 +15,7 @@ def token_response(token: str):
 
 
 def decode_token(token: str) -> dict:
-    decoded_token = jwt.decode(token, JWT_SECRET, audience=JWT_AUDIENCE, algorithms=[JWT_ALGORITHM])
+    jwks_client = PyJWKClient(OIDC_CERTS_URL)
+    signing_key = jwks_client.get_signing_key_from_jwt(token)
+    decoded_token = jwt.decode(token, signing_key.key, audience=JWT_AUDIENCE, algorithms=[JWT_ALGORITHM])
     return decoded_token if decoded_token["exp"] >= time.time() else None
