fc464f412f2e29639ee5444f5b0fcd272835023f
Ana Rute Mendes <ana@igalia.com>
2023-08-16 12:59:54 +0200

    Add projects service in the API

--- api/routers/v1/projects.py
+++ api/routers/v1/projects.py
@@ -1,17 +1,21 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from models.project import Project
 from schemas.project import Project as ProjectSchema
+from services.projects import ProjectService
+
 from db.db_connection import get_db
 from auth.auth_bearer import BearerToken
 
 router = APIRouter(prefix="/projects", tags=["projects"])
 
 
-@router.get("/", dependencies=[Depends(BearerToken())], response_model=list[ProjectSchema])
+# TODO implement pagination
+@router.get("/", dependencies=[Depends(BearerToken())], response_model=List[ProjectSchema])
 async def get_projects(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
-    return db.query(Project).offset(skip).limit(limit).all()
+    return ProjectService(db).get_items()
 
 
 @router.get("/{project_id}", dependencies=[Depends(BearerToken())], response_model=ProjectSchema)
