8ff647bc75b6568b17b99fd3495d50a7e7804c79
Danielle Mayabb <danielle@igalia.com>
2023-09-13 08:18:31 -0700

    Add API endpoint to GET user templates
    - Add service to fetch all user templates plus global templates
    - Add schema

--- api/routers/v1/timelog.py
+++ api/routers/v1/timelog.py
@@ -2,8 +2,8 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from schemas.timelog import TaskTypeItem
-from services.timelog import TaskTypeService
+from schemas.timelog import TaskTypeItem, Template as TemplateSchema
+from services.timelog import TaskTypeService, TemplateService
 
 from db.db_connection import get_db
 from auth.auth_bearer import BearerToken
@@ -15,3 +15,9 @@ router = APIRouter(prefix="/timelog", tags=["timelog"])
 async def get_task_types(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     items = TaskTypeService(db).get_items()
     return items
+
+
+@router.get("/templates/{user_id}", dependencies=[Depends(BearerToken())], response_model=List[TemplateSchema])
+async def get_user_templates(user_id: int, db: Session = Depends(get_db)):
+    templates = TemplateService(db).get_user_templates(user_id)
+    return templates
