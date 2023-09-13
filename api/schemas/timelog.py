d7d223fda90131d081db7437e15e20e366bd8b17
Danielle Mayabb <danielle@igalia.com>
2023-09-13 08:18:31 -0700

    Add API endpoint to GET user templates
    - Add service to fetch all user templates plus global templates
    - Add schema

--- api/schemas/timelog.py
+++ api/schemas/timelog.py
@@ -1,10 +1,28 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class TaskTypeItem(BaseModel):
     slug: str
     name: str
     active: bool
+
+    class Config:
+        orm_mode = True
+
+
+class Template(BaseModel):
+    id: int
+    name: str
+    story: Optional[str]
+    description: Optional[str]
+    task_type: Optional[str]
+    init_time: Optional[int]
+    end_time: Optional[int]
+    customer_id: Optional[int]
+    user_id: Optional[int]
+    project_id: Optional[int]
+    is_global: bool
 
     class Config:
         orm_mode = True
--- api/services/timelog.py
+++ api/services/timelog.py
@@ -1,10 +1,19 @@
 from typing import List
+from sqlalchemy import or_
 
 from services.main import AppService
-from models.timelog import TaskType
+from models.timelog import TaskType, Template
 
 
 class TaskTypeService(AppService):
     def get_items(self) -> List[TaskType]:
         task_types = self.db.query(TaskType).all() or []
         return task_types
+
+
+class TemplateService(AppService):
+    def get_user_templates(self, user_id: int) -> List[Template]:
+        templates = (
+            self.db.query(Template).filter(or_(Template.user_id == user_id, Template.is_global is True)).all() or []
+        )
+        return templates
