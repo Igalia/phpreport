4557ed4ecfd89d572b08e0d13ec2ec5615bdeac6
Danielle Mayabb <danielle@igalia.com>
2023-10-04 14:15:38 -0700

    Add PUT template endpoint
    Closes #681
    - Fix regression where template model validator was lost in upgrade to Pydantic
    - Add endpoint and basic checks: same user checks as for adding a template, check that template user is seeking to edit exists
    - Add simple schema for update
    - Add service methods to get single template and to update
    - Add additional validation for templates to check if there is a task_type and if there is to make sure it exists (otherwise there is a FK error)
    - Refactors TaskValiate object into a more general object that can be used for any validation
    - Moves template validation into its own method

--- api/services/timelog.py
+++ api/services/timelog.py
@@ -1,10 +1,12 @@
 from typing import List
 from datetime import date, datetime
 from sqlalchemy import or_
+from fastapi.encoders import jsonable_encoder
 
 from services.main import AppService
 from models.timelog import TaskType, Template, Task
-from schemas.timelog import TemplateNew, TaskNew, TaskValidate
+from schemas.timelog import TemplateNew, TemplateUpdate, TaskNew
+from schemas.validation import ValidatedObject
 
 
 class TaskTypeService(AppService):
@@ -23,6 +25,10 @@ class TemplateService(AppService):
         )
         return templates
 
+    def get_template(self, template_id: int) -> Template:
+        template = self.db.query(Template).where(Template.id == template_id).first() or None
+        return template
+
     def create_template(self, template: TemplateNew) -> Template:
         new_template = Template(
             name=template.name,
@@ -38,6 +44,17 @@ class TemplateService(AppService):
         self.db.commit()
         self.db.refresh(new_template)
         return new_template
+
+    def update_template(self, existing_template: Template, template_updates: TemplateUpdate) -> Template:
+        existing_data = jsonable_encoder(existing_template)
+        update_data = template_updates.dict(exclude_unset=True)
+        for field in existing_data:
+            if field in update_data:
+                setattr(existing_template, field, update_data[field])
+        self.db.add(existing_template)
+        self.db.commit()
+        self.db.refresh(existing_template)
+        return existing_template
 
 
 class TaskService(AppService):
@@ -69,20 +86,20 @@ class TaskService(AppService):
         self.db.refresh(new_task)
         return new_task
 
-    def check_task_for_overlap(self, task: Task) -> TaskValidate:
-        validated_task = TaskValidate(is_task_valid=True, message="")
+    def check_task_for_overlap(self, task: Task) -> ValidatedObject:
+        validated_task = ValidatedObject(is_valid=True, message="")
         user_tasks_for_day = self.db.query(Task).where(Task.user_id == task.user_id, Task.date == task.date).all() or []
         if len(user_tasks_for_day) <= 0:
             return validated_task
         for user_task_for_day in user_tasks_for_day:
             if task.init == user_task_for_day.init:
-                validated_task.is_task_valid = False
+                validated_task.is_valid = False
                 validated_task.message += f"You have already logged a task beginning at {user_task_for_day.start_time}."
             if task.end == user_task_for_day.end:
-                validated_task.is_task_valid = False
+                validated_task.is_valid = False
                 validated_task.message += f"You have already logged a task ending at {user_task_for_day.end_time}."
             if task.end > user_task_for_day.init and task.init < user_task_for_day.end:
-                validated_task.is_task_valid = False
+                validated_task.is_valid = False
                 validated_task.message += (
                     f"Task from {task.start_time} to {task.end_time} overlaps an existing task from"
                     f" {user_task_for_day.start_time} to {user_task_for_day.end_time}."
