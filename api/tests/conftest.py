279acea9dea3a527abd9cc7ae6be28678fd292fb
Ana Rute Mendes <ana@igalia.com>
2023-10-12 18:40:52 +0200

    Add initial mock data on tests setup

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,20 +1,17 @@
-from typing import Generator
-
+from typing import Dict, Generator
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, StaticPool
 
 from api.main import app
+from db import base  # noqa
+from db.base_class import Base
+from db.db_connection import engine
+from tests.utils.mock_data import DATA
 
-SQLALCHEMY_DATABASE_URL = "sqlite://"
+test_engine = engine
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
 
 @pytest.fixture(scope="session")
@@ -30,3 +27,25 @@ def db() -> Generator:
 def client() -> Generator:
     with TestClient(app) as c:
         yield c
+
+
+@pytest.fixture(autouse=True)
+def init_db():
+    # Drop and create all the tables to start
+    # with the db clean before the tests
+    Base.metadata.drop_all(bind=test_engine)
+    Base.metadata.create_all(bind=test_engine)
+    db = TestingSessionLocal()
+
+    # Clean up tables before importing the initial data
+    for model, entries in DATA:
+        # Insert the data
+        for entry in entries:
+            new_record = model(**entry)
+            db.add(new_record)
+            db.commit()
+            db.refresh(new_record)
+
+    yield
+
+    db.close()
---
+++ api/tests/utils/mock_data.py
@@ -0,0 +1,44 @@
+from models.area import Area
+from models.customer import Customer
+from models.project import Project
+from models.user import User, UserGroup, UserRoles
+from models.sector import Sector
+
+
+DATA = [
+    (
+        User,
+        [
+            {"id": 1, "login": "user", "password": "user"},
+            {"id": 2, "login": "admin", "password": "admin"},
+            {"id": 3, "login": "manager", "password": "manager"},
+        ],
+    ),
+    (UserGroup, [{"id": 1, "name": "staff"}, {"id": 2, "name": "admin"}, {"id": 3, "name": "manager"}]),
+    (
+        UserRoles,
+        [
+            {"group_id": 1, "user_id": 1},
+            {"group_id": 1, "user_id": 2},
+            {"group_id": 2, "user_id": 2},
+            {"group_id": 1, "user_id": 3},
+            {"group_id": 3, "user_id": 3},
+        ],
+    ),
+    (
+        Area,
+        [{"id": 1, "name": "internal"}],
+    ),
+    (
+        Sector,
+        [{"id": 1, "name": "tech"}],
+    ),
+    (
+        Customer,
+        [{"id": 1, "name": "Internal Customer", "customer_type": "Small", "sector_id": 1}],
+    ),
+    (
+        Project,
+        [{"id": 1, "description": "Holidays", "area_id": 1, "customer_id": 1, "is_active": True}],
+    ),
+]
