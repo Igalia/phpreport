295c12828c207bb5c1d4656858b34e07ac24434d
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-10-17 10:35:33 -0300

    Add next-auth to project

---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import KeycloakProvider from 'next-auth/providers/keycloak'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.OIDC_CLIENT_ID!,
+      clientSecret: process.env.OIDC_CLIENT_SECRET!,
+      issuer: process.env.OIDC_AUTHORITY
+    })
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      console.log({ url, baseUrl })
+
+      return `${baseUrl}/web/v2/`
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      session.user.id = token.id
+
+      return session
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        token.accessToken = account.access_token
+        token.id = profile.id
+      }
+      return token
+    }
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
