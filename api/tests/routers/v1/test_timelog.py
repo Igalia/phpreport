e6257880f866f15400dd1f0ab46027abf24a6cb9
Ana Rute Mendes <ana@igalia.com>
2023-10-27 13:56:44 +0200

    Add set of tests for Tasks endpoints

--- api/tests/routers/v1/test_timelog.py
+++ api/tests/routers/v1/test_timelog.py
@@ -10,6 +10,7 @@ def test_get_task_types_authenticated(client: TestClient, get_regular_user_token
     expected_types = [
         {"slug": "meeting", "name": "Meeting", "active": True},
         {"slug": "deprecated", "name": "Deprecated Type", "active": False},
+        {"slug": "project", "name": "Project time", "active": True},
     ]
 
     response = client.get(
@@ -289,3 +290,234 @@ def test_cannot_delete_other_global_template(
     assert response.status_code == HTTPStatus.FORBIDDEN
     res = response.json()
     assert res["detail"] == "You are not authorized to delete global templates"
+
+
+def test_get_user_tasks(client: TestClient, get_regular_user_token_headers: Dict[str, str]) -> None:
+    expected_tasks = [
+        {
+            "id": 1,
+            "user_id": 1,
+            "date": "2023-10-20",
+            "description": "Working in that awesome project",
+            "start_time": "20:00",
+            "end_time": "22:00",
+            "project_id": 2,
+            "project_name": "Internal",
+            "customer_name": "Internal Customer",
+            "story": "that project",
+            "task_type": "project",
+        },
+    ]
+
+    response = client.get(
+        f"{API_BASE_URL}/v1/timelog/tasks/",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1, "start": "2023-10-20", "end": "2023-10-20"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    tasks = response.json()
+    assert tasks == expected_tasks
+
+
+def test_regular_user_cannot_get_other_users_tasks(
+    client: TestClient, get_regular_user_token_headers: Dict[str, str]
+) -> None:
+    response = client.get(
+        f"{API_BASE_URL}/v1/timelog/tasks/",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 2, "start": "2023-10-20", "end": "2023-10-20"},
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    res = response.json()
+    assert res["detail"] == "You are not authorized to view tasks for this user"
+
+
+def test_create_task(client: TestClient, get_regular_user_token_headers: Dict[str, str]) -> None:
+    task_payload = {
+        "date": "2023-10-21",
+        "story": "project",
+        "description": "Adding tests",
+        "task_type": "project",
+        "project_id": 1,
+        "user_id": 1,
+        "start_time": "08:30",
+        "end_time": "14:00",
+    }
+    response = client.get(
+        f"{API_BASE_URL}/v1/timelog/tasks/",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1, "start": "2023-10-21", "end": "2023-10-21"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    tasks = response.json()
+    assert tasks == []
+
+    response = client.post(
+        f"{API_BASE_URL}/v1/timelog/tasks",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1},
+        json=task_payload,
+    )
+    assert response.status_code == HTTPStatus.CREATED
+
+    expected_response = {
+        "id": 3,
+        "date": "2023-10-21",
+        "story": "project",
+        "description": "Adding tests",
+        "task_type": "project",
+        "project_id": 1,
+        "user_id": 1,
+        "start_time": "8:30",
+        "end_time": "14:00",
+        "project_name": "Holidays",
+        "customer_name": "Internal Customer",
+    }
+    assert response.json() == expected_response
+
+    response = client.get(
+        f"{API_BASE_URL}/v1/timelog/tasks/",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1, "start": "2023-10-21", "end": "2023-10-21"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    tasks = response.json()
+    assert tasks == [expected_response]
+
+
+def test_regular_user_cannot_create_tasks_for_other_users(
+    client: TestClient, get_regular_user_token_headers: Dict[str, str]
+) -> None:
+    task_payload = {
+        "date": "2023-10-21",
+        "story": "project",
+        "description": "Adding tests",
+        "task_type": "project",
+        "project_id": 1,
+        "user_id": 2,
+        "start_time": "08:30",
+        "end_time": "14:00",
+    }
+    response = client.post(
+        f"{API_BASE_URL}/v1/timelog/tasks",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1},
+        json=task_payload,
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    res = response.json()
+    assert res["detail"] == "You are not authorized to create tasks for this user."
+
+
+def test_update_task(client: TestClient, get_regular_user_token_headers: Dict[str, str]) -> None:
+    task_payload = {
+        "date": "2023-10-22",
+        "start_time": "10:00",
+        "end_time": "12:00",
+        "user_id": 1,
+    }
+
+    response = client.put(
+        f"{API_BASE_URL}/v1/timelog/tasks/1",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1},
+        json=task_payload,
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+    expected_task = {
+        "id": 1,
+        "user_id": 1,
+        "date": "2023-10-22",
+        "description": "Working in that awesome project",
+        "start_time": "10:00",
+        "end_time": "12:00",
+        "project_id": 2,
+        "project_name": "Internal",
+        "customer_name": "Internal Customer",
+        "story": "that project",
+        "task_type": "project",
+    }
+
+    res = response.json()
+    assert res == expected_task
+
+
+def test_delete_task(client: TestClient, get_regular_user_token_headers: Dict[str, str]) -> None:
+    # Check existing tasks first
+    response = client.get(
+        f"{API_BASE_URL}/v1/timelog/tasks/",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1, "start": "2023-10-20", "end": "2023-10-20"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    tasks = response.json()
+    assert len(tasks) == 1
+
+    response = client.delete(
+        f"{API_BASE_URL}/v1/timelog/tasks/1",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1},
+    )
+    assert response.status_code == HTTPStatus.NO_CONTENT
+
+    # After the deletion there should be no task for this user in this day
+    response = client.get(
+        f"{API_BASE_URL}/v1/timelog/tasks/",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1, "start": "2023-10-20", "end": "2023-10-20"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    tasks = response.json()
+    assert tasks == []
+
+
+def test_regular_user_cannot_delete_other_users_tasks(
+    client: TestClient, get_regular_user_token_headers: Dict[str, str]
+) -> None:
+    response = client.delete(
+        f"{API_BASE_URL}/v1/timelog/tasks/2",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1},
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    res = response.json()
+    assert res["detail"] == "You are not authorized to delete tasks for this user"
+
+
+def test_delete_nonexistent_task(client: TestClient, get_regular_user_token_headers: Dict[str, str]) -> None:
+    response = client.delete(
+        f"{API_BASE_URL}/v1/timelog/tasks/123",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1, "start": "2023-10-20", "end": "2023-10-20"},
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+    res = response.json()
+    assert res["detail"] == "Task with id 123 not found"
+
+
+def test_user_cannot_create_task_with_overlapping_hours(
+    client: TestClient, get_regular_user_token_headers: Dict[str, str]
+) -> None:
+    task_payload = {
+        "date": "2023-10-20",
+        "story": "project",
+        "description": "Overlapping time with task 1",
+        "task_type": "project",
+        "project_id": 1,
+        "user_id": 1,
+        "start_time": "20:30",
+        "end_time": "23:00",
+    }
+
+    response = client.post(
+        f"{API_BASE_URL}/v1/timelog/tasks",
+        headers=get_regular_user_token_headers,
+        params={"user_id": 1},
+        json=task_payload,
+    )
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    error_message = response.json()
+    assert error_message["detail"] == "Task from 20:30 to 23:00 overlaps an existing task from 20:00 to 22:00."
