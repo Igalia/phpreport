7a867c31ab5b6e997fa65bc7b950724db729a738
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-11-09 16:12:00 -0300

    Add Zod and create Task validation schema
    Zod is a library for schema and validation that works very well with
    typescript, it allow us to create the schemas for the domain objects
    and infer the type from it making our lifes easier

--- frontend/src/domain/Task.ts
+++ frontend/src/domain/Task.ts
@@ -1,13 +1,60 @@
+import { z } from 'zod'
+
 export type Task = {
   id: number
   userId: number
-  projectId: string
-  taskType: string
+  projectId: number
+  taskType?: string | null
   story: string
   description: string
   startTime: string
   endTime: string
   date: string
+  init: number
+  end: number
+  projectName: string
+  customerName: string
 }
 
-export type TaskIntent = Omit<Task, 'id'>
+export const TaskIntent = z.object({
+  userId: z.number(),
+  projectId: z.string().min(1, { message: 'Project is required' }),
+  taskType: z.string(),
+  story: z.string(),
+  description: z.string(),
+  startTime: z.string().min(1, { message: 'startTime is required' }),
+  endTime: z.string().min(1, { message: 'endTime is required ' }),
+  date: z.string()
+})
+
+export type TaskIntent = z.infer<typeof TaskIntent>
+
+function getMinutes(time: string) {
+  const hours = Number(time.split(':')[0])
+  const minutes = Number(time.split(':')[1])
+  return hours * 60 + minutes
+}
+
+export const getOverlappingTasks = (newTask: TaskIntent, tasks: Array<Task>) => {
+  let message = ''
+
+  const overlappingTasks = tasks.filter((task) => {
+    const haveEqualDate = task.date === newTask.date
+    const overlapsTasksDuration =
+      getMinutes(newTask.endTime) > getMinutes(task.startTime) &&
+      getMinutes(newTask.startTime) < getMinutes(task.endTime)
+    const coincidesInitOrEndTimes =
+      getMinutes(newTask.startTime) == getMinutes(task.startTime) ||
+      getMinutes(newTask.endTime) == getMinutes(task.endTime)
+
+    const isOverlapping = haveEqualDate && (overlapsTasksDuration || coincidesInitOrEndTimes)
+
+    if (isOverlapping) {
+      message += `Task from ${newTask.startTime} to ${newTask.endTime} overlaps with task from ${task.startTime} to ${task.endTime}. `
+    }
+
+    return isOverlapping
+  })
+
+  return { overlappingTasks, message }
+}
