a2424fe3ebd90c3f851262fac7468fbf035ee656
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-11-14 10:41:08 -0300

    Add delete button to task

--- frontend/src/app/tasks/TaskList.tsx
+++ frontend/src/app/tasks/TaskList.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import Box from '@mui/joy/Box'
+import Button from '@mui/joy/Button'
 import Typography from '@mui/joy/Typography'
-
-import { useGetTasks } from './hooks/useTask'
+import { Delete24Filled } from '@fluentui/react-icons'
+import { useGetTasks, useDeleteTask } from './hooks/useTask'
+import { styled } from '@mui/joy/styles'
+import { ScreenReaderOnly } from '@/ui/ScreenReaderOnly/ScreenReaderOnly'
 
 type TaskListProps = {
   userId: number
@@ -26,6 +29,7 @@ const getTimeDifference = (startTime: string, endTime: string) => {
 
 export const TaskList = ({ userId }: TaskListProps) => {
   const tasks = useGetTasks(userId)
+  const { deleteTask } = useDeleteTask(userId)
 
   return (
     <Box
@@ -34,7 +38,8 @@ export const TaskList = ({ userId }: TaskListProps) => {
         maxWidth: { xs: '100%', sm: '558px' },
         display: 'flex',
         flexDirection: 'column',
-        gap: '16px'
+        gap: '16px',
+        listStyle: 'none'
       }}
     >
       {tasks.map((task) => (
@@ -43,23 +48,40 @@ export const TaskList = ({ userId }: TaskListProps) => {
             backgroundColor: '#A8AEEB',
             height: '96px',
             width: { xs: '100%', sm: '558px' },
-            display: 'flex',
-            flexDirection: 'column',
             borderRadius: '8px',
-            padding: '16px'
+            padding: '16px',
+            display: 'flex',
+            justifyContent: 'space-between'
           }}
           key={task.id}
           component="li"
         >
-          <Typography textColor="#1E2AA5">
-            {task.projectName} - {task.customerName}
-          </Typography>
-          <Typography textColor="#1E2AA5">{task.taskType}</Typography>
-          <Typography textColor="#1E2AA5" fontWeight="bold">
-            {task.startTime}-{task.endTime} ({getTimeDifference(task.startTime, task.endTime)})
-          </Typography>
+          <Box display="flex" flexDirection="column">
+            <Typography textColor="#1E2AA5">
+              {task.projectName} - {task.customerName}
+            </Typography>
+            <Typography textColor="#1E2AA5">{task.taskType}</Typography>
+            <Typography textColor="#1E2AA5" fontWeight="bold">
+              {task.startTime}-{task.endTime} ({getTimeDifference(task.startTime, task.endTime)})
+            </Typography>
+          </Box>
+          <Box display="flex" gap="24px">
+            <IconButton onClick={() => deleteTask(task.id)}>
+              <Delete24Filled color="#2f3338" />
+              <ScreenReaderOnly>Delete task {task.id}</ScreenReaderOnly>
+            </IconButton>
+          </Box>
         </Box>
       ))}
     </Box>
   )
 }
+
+const IconButton = styled(Button)`
+  background: transparent;
+  padding: 0;
+
+  &:hover {
+    background: transparent;
+  }
+`
