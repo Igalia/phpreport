a9aa719cba9217467b252af36067b81fe1dc3397
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-11-17 12:34:56 -0300

    Add useGetCurrentUser mock to TaskForm test
    The moduleNameMapper was added to jest.config to be able to use
    absolute imports with jest.mock()

--- frontend/jest.config.mjs
+++ frontend/jest.config.mjs
@@ -1,13 +1,16 @@
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir: './'
 })
 
 const config = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 }
 
 export default createJestConfig(config)
