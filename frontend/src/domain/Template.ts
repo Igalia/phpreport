6a7393527195b35afd2fa287f9a8e591e257da6e
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-11-17 16:59:15 -0300

    Add getTemplate function to project
    This adds the Template domain object and the function to fetch
    templates from the api

---
+++ frontend/src/domain/Template.ts
@@ -0,0 +1,12 @@
+export type Template = {
+  id: number
+  name: string
+  story: string
+  description: string
+  taskType: string
+  userId: number
+  projectId: number
+  isGlobal: boolean
+  startTime: string
+  endTime: string
+}
