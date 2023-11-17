e70c449f4401d7a2cf0f6aa6896e31d5841c5a7d
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-11-17 17:01:44 -0300

    Add template fetch to tasks page

--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -4,15 +4,24 @@ import { TaskForm } from './TaskForm'
 import { getProjects } from '@/infra/project/getProjects'
 import { getTaskTypes } from '@/infra/taskType/getTaskTypes'
 import { serverFetch } from '@/infra/lib/serverFetch'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
+import { getTemplates } from '@/infra/template/getTemplates'
 
 const getPageData = async () => {
   const apiClient = await serverFetch()
+  const session = await getServerSession(authOptions)
+  const { id: userId } = session!.user
 
-  return await Promise.all([getProjects(apiClient), getTaskTypes(apiClient)])
+  return await Promise.all([
+    getProjects(apiClient),
+    getTaskTypes(apiClient),
+    getTemplates(apiClient, { userId })
+  ])
 }
 
 export default async function Tasks() {
-  const [projects, taskTypes] = await getPageData()
+  const [projects, taskTypes, templates] = await getPageData()
 
   return (
     <Box
