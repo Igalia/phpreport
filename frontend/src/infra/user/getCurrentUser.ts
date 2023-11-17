bfc5d50ca48e64ce5011c75bd935de381ebeeaa0
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-11-17 12:21:34 -0300

    Add user to session data
    This merges the default user from session with the user data that we
    fetch from the api

--- frontend/src/infra/user/getCurrentUser.ts
+++ frontend/src/infra/user/getCurrentUser.ts
@@ -1,26 +1,12 @@
 import { User } from '@/domain/User'
 import { ApiClient } from '../lib/apiClient'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 export const getCurrentUser = async (apiClient: ApiClient): Promise<User> => {
-  const [session, response] = await Promise.all([
-    getServerSession(authOptions),
-    apiClient('/v1/users/me')
-  ])
-
-  if (!session) {
-    throw new Error('Failed to fetch Session')
-  }
+  const response = await apiClient('/v1/users/me')
 
   if (!response.ok) {
     throw new Error('Failed to fetch Current User')
   }
 
-  const user = await response.json()
-
-  return {
-    ...session.user,
-    ...user
-  }
+  return await response.json()
 }
