183092432e3e793a65360f0b087d8daea70505b2
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-06 09:32:18 -0300

    Fix useGetCurrentUser breaking on server
    If session data doesn't exist, it only returns an empty object

--- frontend/src/hooks/useGetCurrentUser/useGetCurrentUser.ts
+++ frontend/src/hooks/useGetCurrentUser/useGetCurrentUser.ts
@@ -1,9 +1,14 @@
 import { useSession } from 'next-auth/react'
+import { Session } from 'next-auth'
 
 export const useGetCurrentUser = () => {
   const { data } = useSession({
     required: true
   })
 
-  return data!.user
+  if (!data) {
+    return {} as Session['user']
+  }
+
+  return data.user
 }
