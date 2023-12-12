f39fa8762746f06732984870a5981d6f42e38272
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-12 11:26:48 -0300

    Improve schema validation for Task type

--- frontend/src/domain/Task.ts
+++ frontend/src/domain/Task.ts
@@ -1,37 +1,55 @@
 import { z } from 'zod'
 
-export type Task = {
-  id: number
-  userId: number
-  projectId: number
-  taskType?: string | null
-  story: string
-  description: string
-  startTime: string
-  endTime: string
-  date: string
-  projectName: string
-  customerName: string
-}
-
-export const TaskIntent = z.object({
-  userId: z.number(),
-  projectId: z.string().min(1, { message: 'Project is required' }),
-  taskType: z.string(),
-  story: z.string(),
-  description: z.string(),
-  startTime: z.string().min(1, { message: 'startTime is required' }),
-  endTime: z.string().min(1, { message: 'endTime is required ' }),
-  date: z.string()
-})
-
-export type TaskIntent = z.infer<typeof TaskIntent>
-
 function getMinutes(time: string) {
   const hours = Number(time.split(':')[0])
   const minutes = Number(time.split(':')[1])
   return hours * 60 + minutes
 }
+
+const timeValidation = (startTime: string, endTime: string, ctx: z.RefinementCtx) => {
+  if (getMinutes(startTime) > getMinutes(endTime)) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'End time must be after Start time'
+    })
+  }
+}
+
+export const Task = z
+  .object({
+    id: z.number(),
+    userId: z.number(),
+    projectId: z.number().min(1, { message: 'Project is required' }),
+    taskType: z.string().nullable(),
+    story: z.string(),
+    description: z.string(),
+    startTime: z.string().min(1, { message: 'Start time is required' }),
+    endTime: z.string().min(1, { message: 'End time is required ' }),
+    date: z.string(),
+    customerName: z.string(),
+    projectName: z.string()
+  })
+  .superRefine((obj, ctx) => {
+    timeValidation(obj.startTime, obj.endTime, ctx)
+  })
+
+export type Task = z.infer<typeof Task>
+
+export const TaskIntent = z
+  .object({
+    userId: z.number(),
+    projectId: z.string().min(1, { message: 'Project is required' }),
+    taskType: z.string().optional(),
+    story: z.string(),
+    description: z.string(),
+    startTime: z.string().min(1, { message: 'Start time is required' }),
+    endTime: z.string().min(1, { message: 'End time is required ' }),
+    date: z.string()
+  })
+  .superRefine((obj, ctx) => {
+    timeValidation(obj.startTime, obj.endTime, ctx)
+  })
+export type TaskIntent = z.infer<typeof TaskIntent>
 
 export const getOverlappingTasks = (newTask: TaskIntent, tasks: Array<Task>) => {
   let message = ''
