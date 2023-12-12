6a25f46a3effecc37796b8f773a6fcea68765e04
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-12 11:21:35 -0300

    Rename Task component to TaskBox
    Also moved the useDeletTask hook call to insided of component and added
    edit icon to set task edition

--- frontend/src/app/tasks/components/TaskBox.tsx
+++ frontend/src/app/tasks/components/TaskBox.tsx
@@ -4,7 +4,10 @@ import { useState } from 'react'
 import Box from '@mui/joy/Box'
 import Button from '@mui/joy/Button'
 import Typography from '@mui/joy/Typography'
-import { Delete24Filled } from '@fluentui/react-icons'
+import { Delete24Filled, Edit24Filled } from '@fluentui/react-icons'
+
+import { Project } from '@/domain/Project'
+import { TaskType } from '@/domain/TaskType'
 
 import { ScreenReaderOnly } from '@/ui/ScreenReaderOnly/ScreenReaderOnly'
 import { ConfirmationModal } from '@/ui/ConfirmationModal/ConfirmationModal'
@@ -12,13 +15,21 @@ import { styled } from '@mui/joy/styles'
 import { Task } from '@/domain/Task'
 import { getTimeDifference, convertTimeToMinutes } from '../utils/time'
 
+import { EditTask } from './EditTask'
+import { useDeleteTask } from '../hooks/useTask'
+
 type TaskProps = {
   task: Task
-  deleteTask: (taskId: number) => void
+  tasks: Array<Task>
+  projects: Array<Project>
+  taskTypes: Array<TaskType>
 }
 
-export const TaskBox = ({ task, deleteTask }: TaskProps) => {
+export const TaskBox = ({ task, projects, taskTypes, tasks }: TaskProps) => {
+  const { deleteTask } = useDeleteTask()
+
   const [deleteModalOpen, setDeleteModalOpen] = useState(false)
+  const [editMode, setEditMode] = useState(false)
 
   const timeDifference = () => {
     const startMinutes = convertTimeToMinutes(task.startTime)
@@ -31,7 +42,7 @@ export const TaskBox = ({ task, deleteTask }: TaskProps) => {
     <Box
       sx={{
         backgroundColor: '#A8AEEB',
-        height: '96px',
+        minHeight: '96px',
         width: { xs: '100%', sm: '558px' },
         borderRadius: '8px',
         padding: '16px',
@@ -40,16 +51,32 @@ export const TaskBox = ({ task, deleteTask }: TaskProps) => {
       }}
       component="li"
     >
-      <Box display="flex" flexDirection="column">
-        <Typography textColor="#1E2AA5">
-          {task.projectName} - {task.customerName}
-        </Typography>
-        <Typography textColor="#1E2AA5">{task.taskType}</Typography>
-        <Typography textColor="#1E2AA5" fontWeight="bold">
-          {task.startTime}-{task.endTime} ({timeDifference()})
-        </Typography>
+      <Box display="flex" flexDirection="column" gap={editMode ? '8px' : 0}>
+        {editMode ? (
+          <EditTask
+            closeForm={() => setEditMode(false)}
+            tasks={tasks}
+            task={task}
+            projects={projects}
+            taskTypes={taskTypes}
+          />
+        ) : (
+          <>
+            <Typography textColor="#1E2AA5">
+              {task.projectName} - {task.customerName}
+            </Typography>
+            <Typography textColor="#1E2AA5">{task.taskType}</Typography>
+            <Typography textColor="#1E2AA5" fontWeight="bold">
+              {task.startTime}-{task.endTime} ({timeDifference()})
+            </Typography>
+          </>
+        )}
       </Box>
-      <Box display="flex" gap="24px">
+      <Box display="flex" gap="8px">
+        <IconButton onClick={() => setEditMode(true)}>
+          <Edit24Filled color="#2f3338" />
+          <ScreenReaderOnly>Edit task</ScreenReaderOnly>
+        </IconButton>
         <IconButton onClick={() => setDeleteModalOpen(true)}>
           <Delete24Filled color="#2f3338" />
           <ScreenReaderOnly>Delete task {task.id}</ScreenReaderOnly>
@@ -73,6 +100,8 @@ export const TaskBox = ({ task, deleteTask }: TaskProps) => {
 const IconButton = styled(Button)`
   background: transparent;
   padding: 0;
+  height: 24px;
+  width: 24px;
 
   &:hover {
     background: transparent;
