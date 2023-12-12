57d734e683c33ec72a8c6d289f00c9bdd55728d6
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-12 11:28:47 -0300

    Create EditTask component

---
+++ frontend/src/app/tasks/hooks/useEditTaskForm.ts
@@ -0,0 +1,55 @@
+import { useCallback } from 'react'
+
+import { useAlert } from '@/ui/Alert/useAlert'
+import { useForm } from '@/hooks/useForm/useForm'
+import { Task, getOverlappingTasks } from '@/domain/Task'
+import { useEditTask } from './useTask'
+import { Project } from '@/domain/Project'
+
+type UseEditTaskFormProps = {
+  task: Task
+  tasks: Array<Task>
+  closeForm: () => void
+}
+
+export const useEditTaskForm = ({ task, tasks, closeForm }: UseEditTaskFormProps) => {
+  const { formState, handleChange, resetForm } = useForm<Task>({
+    initialValues: task
+  })
+  const { showError } = useAlert()
+  const { editTask } = useEditTask({ handleSuccess: closeForm })
+
+  const handleSubmit = useCallback(() => {
+    const validation = Task.safeParse(formState)
+
+    if (!validation.success) {
+      validation.error.issues.map(({ message }) => {
+        showError(message)
+      })
+
+      return
+    }
+
+    const { message } = getOverlappingTasks(
+      formState,
+      tasks.filter(({ id }) => id !== formState.id)
+    )
+
+    if (message.length > 0) {
+      showError(message)
+      return
+    }
+
+    editTask(formState)
+  }, [editTask, formState, showError, tasks])
+
+  const handleProject = (value: string, projects: Array<Project>) => {
+    const project = projects.find((project) => project.description === value)
+    if (project) {
+      handleChange('projectId', project.id)
+      handleChange('projectName', value)
+    }
+  }
+
+  return { formState, handleChange, handleSubmit, resetForm, handleProject }
+}
