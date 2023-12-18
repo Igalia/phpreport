3cca695e1a96faa1f8bd632999c17a1203985699
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-18 11:26:45 -0300

    Rename TaskForm to DayView
    This change is to avoid confunsion on what is the task creation form
    and is not part of the form

---
+++ frontend/src/app/tasks/DayView.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Box from '@mui/joy/Box'
+
+import Divider from '@mui/joy/Divider'
+
+import { Project } from '@/domain/Project'
+import { TaskType } from '@/domain/TaskType'
+import { Template } from '@/domain/Template'
+
+import { TaskList } from './TaskList'
+import { CreateTask } from './components/CreateTask'
+
+type DayViewProps = {
+  projects: Array<Project>
+  taskTypes: Array<TaskType>
+  templates: Array<Template>
+}
+
+export const DayView = ({ projects, taskTypes, templates }: DayViewProps) => {
+  return (
+    <Box
+      sx={{
+        padding: { xs: '0 8px', sm: '0 32px' },
+        display: 'grid',
+        gridTemplateAreas: {
+          xs: "'select-template start-timer''divider divider''create-task-form create-task-form''task-list task-list'",
+          sm: "'select-template start-timer''divider divider''create-task-form task-list'"
+        },
+        gridTemplateColumns: {
+          xs: '1fr',
+          sm: '558px 558px'
+        },
+        margin: '0 auto',
+        maxWidth: '1146px',
+        columnGap: '30px',
+        rowGap: '16px'
+      }}
+    >
+      <CreateTask projects={projects} taskTypes={taskTypes} templates={templates} />
+
+      <Divider sx={{ gridArea: 'divider' }} />
+
+      <TaskList sx={{ gridArea: 'task-list' }} projects={projects} taskTypes={taskTypes} />
+    </Box>
+  )
+}
--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -1,11 +1,10 @@
-import { TaskForm } from './TaskForm'
+import { DayView } from './DayView'
 import { getProjects } from '@/infra/project/getProjects'
 import { getTaskTypes } from '@/infra/taskType/getTaskTypes'
 import { serverFetch } from '@/infra/lib/serverFetch'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
 import { getTemplates } from '@/infra/template/getTemplates'
-import { TaskList } from './TaskList'
 
 const getPageData = async () => {
   const apiClient = await serverFetch()
@@ -22,12 +21,5 @@ const getPageData = async () => {
 export default async function Tasks() {
   const [projects, taskTypes, templates] = await getPageData()
 
-  return (
-    <TaskForm
-      projects={projects}
-      taskTypes={taskTypes}
-      templates={templates}
-      taskList={<TaskList projects={projects} taskTypes={taskTypes} />}
-    />
-  )
+  return <DayView projects={projects} taskTypes={taskTypes} templates={templates} />
 }
