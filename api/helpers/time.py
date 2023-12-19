53b5c4da3371e8949b5544bc105188dc43057520
Ana Rute Mendes <ana@igalia.com>
2023-12-19 11:12:37 -0300

    Add project allocation model

--- api/helpers/time.py
+++ api/helpers/time.py
@@ -9,3 +9,15 @@ def time_string_to_int(time_string: str) -> int:
 def int_to_time_string(time_minutes: int) -> str:
     # Drop the seconds as the default format we are using is HH:mm
     return str(timedelta(minutes=time_minutes))[:-3]
+
+
+def total_hours_between_dates(start_date, end_date, hours_per_weekday):
+    current_date = start_date
+    total_hours = 0
+
+    while current_date <= end_date:
+        if current_date.weekday() < 5:
+            total_hours += hours_per_weekday
+        current_date += timedelta(days=1)
+
+    return total_hours
