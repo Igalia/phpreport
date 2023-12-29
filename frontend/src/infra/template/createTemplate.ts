b12c1453ae16e74675520a084a14131464d4fb77
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-29 17:47:23 -0300

    Create createTemplate api call
    This commit changes the structure for the apiClient dependecy injection,
    it exports a makeCreateTemplate function that receives the apiClient as
    prop and return the createTemplate function, I made this change because
    I think this pattern is easier to follow through the application. Also I
    intend to change the other api calls in future pr's.

---
+++ frontend/src/infra/template/createTemplate.ts
@@ -0,0 +1,34 @@
+import { Template, TemplateIntent } from '@/domain/Template'
+import { ApiClient } from '../lib/apiClient'
+import { BaseError } from '@/_lib/errors/BaseError'
+
+const createTemplateError = (message = 'Failed to create template') => {
+  const name = 'CreateTemplateError'
+
+  return new BaseError({ message, name, code: name })
+}
+
+export const makeCreateTemplate = (apiClient: ApiClient) => async (template: TemplateIntent) => {
+  const validation = TemplateIntent.safeParse(template)
+
+  if (!validation.success) {
+    throw createTemplateError(
+      validation.error.issues.reduce((acc, nextValue) => `${acc} ${nextValue.message}`, '')
+    )
+  }
+
+  try {
+    const response = await apiClient('/v1/timelog/templates', {
+      method: 'POST',
+      body: JSON.stringify(template)
+    })
+
+    if (!response.ok) {
+      throw createTemplateError()
+    }
+
+    return { data: (await response.json()) as Template }
+  } catch (e) {
+    throw createTemplateError()
+  }
+}
