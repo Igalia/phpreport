4fcb9984649a57618f84c09d6a505f8a79496e28
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-12-29 17:43:48 -0300

    Create TemplateIntent schema for validation

--- frontend/src/domain/Template.ts
+++ frontend/src/domain/Template.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 export type Template = {
   id: number
   name: string
@@ -10,3 +12,18 @@ export type Template = {
   startTime: string | null
   endTime: string | null
 }
+
+export const TemplateIntent = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
+  story: z.string().nullable(),
+  description: z.string().nullable(),
+  taskType: z.string().nullable(),
+  projectId: z.number().nullable(),
+  userId: z.number(),
+  isGlobal: z.boolean().nullable(),
+  startTime: z.string().min(1, { message: 'Start time is required' }),
+  endTime: z.string().min(1, { message: 'End time is required ' }),
+  id: z.number().optional()
+})
+
+export type TemplateIntent = z.infer<typeof TemplateIntent>
