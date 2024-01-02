45501654723a17c9b53f31ce0652ef1721c4f02b
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-02 16:50:46 -0300

    Remove "/web/v2" from the auth files

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -20,17 +20,23 @@ export async function middleware(request: NextRequest) {
   const isAccessTokenValid = token?.accessToken && validateToken(token.accessToken)
 
   if (!isAccessTokenValid && !isAllowed) {
-    return NextResponse.redirect(new URL('/web/v2/api/auth/signin', request.url))
+    return NextResponse.redirect(new URL('/api/auth/signin', request.url))
   }
-  if(isAccessTokenValid){
-    if(!token.user?.id){
-      return new NextResponse("You do not have a user record in the application. Please contact your sysadmin.", {status: 401})
+  if (isAccessTokenValid) {
+    if (!token.user?.id) {
+      return new NextResponse(
+        'You do not have a user record in the application. Please contact your sysadmin.',
+        { status: 401 }
+      )
     }
-    if(!token.user.roles || token.user.roles.length ==0){
-      return new NextResponse("You have not been assigned any roles in the application. Please contact your sysadmin.", {status: 403})
+    if (!token.user.roles || token.user.roles.length == 0) {
+      return new NextResponse(
+        'You have not been assigned any roles in the application. Please contact your sysadmin.',
+        { status: 403 }
+      )
     }
-    if(path == "/tasks" && !token.user.authorizedScopes.includes("task:read-own")){
-      return NextResponse.redirect(new URL('/web/v2/auth/error?error=AccessDenied', request.url))
+    if (path == '/tasks' && !token.user.authorizedScopes.includes('task:read-own')) {
+      return NextResponse.redirect(new URL('/auth/error?error=AccessDenied', request.url))
     }
   }
 
