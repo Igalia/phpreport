8bfb3f3c5f630a8decb3c32e22b20af614827809
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-05 12:23:36 -0300

    Move taskForm timer logic to it's own hook
    Since that piece of code is only used by the CreateTask component it is
    being isolated to only be called where it is needed.

---
+++ frontend/src/app/tasks/hooks/useTaskFormTimer.ts
@@ -0,0 +1,53 @@
+import { format } from 'date-fns'
+import { useCallback } from 'react'
+import { TaskIntent } from '@/domain/Task'
+import { useForm } from '@/hooks/useForm/useForm'
+import { useTimer } from '@/hooks/useTimer/useTimer'
+import { convertTimeToMinutes, getTimeDifference } from '../utils/time'
+
+type UseTaskFormTimerProps = {
+  handleChange: ReturnType<typeof useForm<TaskIntent>>['handleChange']
+  startTime: TaskIntent['startTime']
+  endTime: TaskIntent['endTime']
+}
+
+export const useTaskFormTimer = ({ handleChange, startTime, endTime }: UseTaskFormTimerProps) => {
+  const { startTimer, stopTimer, seconds, minutes, hours, isTimerRunning } = useTimer()
+
+  const toggleTimer = useCallback(() => {
+    const onStartTimer = () => {
+      handleChange('startTime', format(new Date(), 'HH:mm'))
+      handleChange('endTime', '')
+
+      startTimer()
+    }
+
+    const onStopTimer = () => {
+      handleChange('endTime', format(new Date(), 'HH:mm'))
+      stopTimer()
+    }
+
+    return isTimerRunning ? onStopTimer() : onStartTimer()
+  }, [handleChange, isTimerRunning, startTimer, stopTimer])
+
+  const loggedTime = useCallback(() => {
+    if (isTimerRunning) {
+      return `${hours}h ${minutes}m ${seconds}s`
+    }
+
+    const startMinutes = convertTimeToMinutes(startTime)
+    const endMinutes = convertTimeToMinutes(endTime)
+
+    if (endMinutes - startMinutes <= 0 || Number.isNaN(endMinutes) || Number.isNaN(startMinutes)) {
+      return '0h 0m 0s'
+    }
+
+    return `${getTimeDifference(startMinutes, endMinutes)} 0s`
+  }, [endTime, hours, isTimerRunning, minutes, seconds, startTime])
+
+  return {
+    loggedTime: loggedTime(),
+    toggleTimer,
+    isTimerRunning
+  }
+}
