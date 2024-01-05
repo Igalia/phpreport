c26722a689d735a55ff0a74d8884b0d96745ac7f
Danielle Mayabb <danielle@igalia.com>
2024-01-05 09:56:56 -0800

    db: Migration/model changes for config
    In preparation for the work summary panel, some values are needed in the config table. We wanted to migrate more values out of the env and into the db and this is a good chance to do it.
    Includes a bump to the pydantic version

--- api/models/config.py
+++ api/models/config.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, Date
+from sqlalchemy import Column, Integer, String, Boolean, Date, CheckConstraint, Numeric
 
 from db.base_class import Base
 
@@ -12,3 +12,7 @@ class Config(Base):
     block_tasks_by_day_limit_number_of_days = Column(Integer, nullable=True)
     block_tasks_by_date_enabled = Column(Boolean, default=False, nullable=False)
     block_tasks_by_date_date = Column(Date, default=False, nullable=True)
+    vacation_project_id = Column(Integer, nullable=True)
+    yearly_vacation_hours = Column(Integer, nullable=True)
+    company_fte = Column(Numeric(precision=8, scale=4), nullable=True)
+    CheckConstraint("id = 1", name="only_one_row_allowed")
