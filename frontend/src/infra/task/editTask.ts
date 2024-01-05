9c2d745fa8730ab27bf21abedd91a4578cecaf75
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-05 12:47:54 -0300

    Move editTask api call to the factory pattern

--- frontend/src/infra/task/editTask.ts
+++ frontend/src/infra/task/editTask.ts
@@ -1,28 +1,34 @@
-import { Task, TaskIntent } from '@/domain/Task'
+import { Task, TaskIntent, validateTask } from '@/domain/Task'
 import { ApiClient } from '../lib/apiClient'
 
-export const editTask = async (task: TaskIntent, apiClient: ApiClient): Promise<Task> => {
-  return apiClient(`/v1/timelog/tasks/${task.id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      user_id: task.userId,
-      project_id: task.projectId,
-      story: task.story,
-      description: task.description,
-      task_type: task.taskType,
-      start_time: task.startTime,
-      end_time: task.endTime,
-      date: task.date
-    })
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText)
-      }
-      return response
-    })
-    .then((response) => response.json() as Promise<Task>)
-    .catch((e) => {
-      throw new Error(e)
-    })
+import { BaseError } from '@/_lib/errors/BaseError'
+
+const editTaskError = (message = 'Failed to edit task') => {
+  const name = 'EditTaskError'
+
+  return new BaseError({ message, name, code: name })
 }
+
+export const makeEditTask =
+  (apiClient: ApiClient) => async (task: TaskIntent, tasks: Array<Task>) => {
+    const validation = validateTask(task, tasks)
+
+    if (!validation.success) {
+      throw editTaskError(validation.message)
+    }
+
+    try {
+      const response = await apiClient(`/v1/timelog/tasks/${task.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(task)
+      })
+
+      if (!response.ok) {
+        throw editTaskError()
+      }
+
+      return (await response.json()) as Task
+    } catch (e) {
+      throw editTaskError()
+    }
+  }
