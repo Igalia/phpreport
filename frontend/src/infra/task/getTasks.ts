656dd109c6e8f0d122757288b1a986f0fbd9a57a
Ana Rute Mendes <ana@igalia.com>
2024-01-15 18:24:48 +0100

    remove slash in url to avoid api redirect

--- frontend/src/infra/task/getTasks.ts
+++ frontend/src/infra/task/getTasks.ts
@@ -6,7 +6,7 @@ export const getTasks = async (
   { userId, start, end }: { userId: number; start: string; end: string }
 ): Promise<Array<Task>> => {
   const params = new URLSearchParams({ user_id: userId.toString(), start, end })
-  const response = await apiClient(`/v1/timelog/tasks/?${params}`)
+  const response = await apiClient(`/v1/timelog/tasks?${params}`)
 
   if (!response.ok) {
     throw new Error('Failed to fetch Tasks')
