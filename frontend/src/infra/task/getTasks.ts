9ad68f953131db9fd318a745dbabad443bc84021
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-19 11:44:21 -0300

    Move getTasks to factory pattern

--- frontend/src/infra/task/getTasks.ts
+++ frontend/src/infra/task/getTasks.ts
@@ -1,16 +1,21 @@
 import { ApiClient } from '@/infra/lib/apiClient'
 import { Task } from '@/domain/Task'
 
-export const getTasks = async (
-  apiClient: ApiClient,
-  { userId, start, end }: { userId: number; start: string; end: string }
-): Promise<Array<Task>> => {
-  const params = new URLSearchParams({ user_id: userId.toString(), start, end })
-  const response = await apiClient(`/v1/timelog/tasks?${params}`)
+type GetTasksProps = Pick<Task, 'userId' | 'startTime' | 'endTime'>
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch Tasks')
+export const makeGetTasks =
+  (apiClient: ApiClient) =>
+  async ({ userId, startTime, endTime }: GetTasksProps): Promise<Array<Task>> => {
+    const params = new URLSearchParams({
+      user_id: userId.toString(),
+      start: startTime,
+      end: endTime
+    })
+    const response = await apiClient(`/v1/timelog/tasks?${params}`)
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch Tasks')
+    }
+
+    return response.json()
   }
-
-  return await response.json()
-}
