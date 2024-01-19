ec0298d2c6314c5810d5aeb3b08afe0d50c7a659
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-19 11:44:21 -0300

    Move getTasks to factory pattern

--- frontend/src/app/tasks/hooks/useGetTasks.ts
+++ frontend/src/app/tasks/hooks/useGetTasks.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { getTasks } from '@/infra/task/getTasks'
+import { makeGetTasks } from '@/infra/task/getTasks'
 import { useClientFetch } from '@/infra/lib/useClientFetch'
 import { format } from 'date-fns'
 import { useGetCurrentUser } from '@/hooks/useGetCurrentUser/useGetCurrentUser'
@@ -8,10 +8,11 @@ export const useGetTasks = () => {
   const apiClient = useClientFetch()
   const { id: userId } = useGetCurrentUser()
   const today = format(new Date(), 'yyyy-MM-dd')
+  const getTasks = makeGetTasks(apiClient)
 
   const { data } = useQuery({
     queryKey: ['tasks', userId],
-    queryFn: () => getTasks(apiClient, { userId, start: today, end: today }),
+    queryFn: () => getTasks({ userId, startTime: today, endTime: today }),
     initialData: []
   })
 
