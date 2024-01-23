eb7b03c6f7709aa8a67a43144c40be3aa01cc399
Ana Rute Mendes <ana@igalia.com>
2024-01-23 16:10:10 +0100

    api: implement get users endpoint

--- api/routers/v1/users.py
+++ api/routers/v1/users.py
@@ -1,9 +1,11 @@
+from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from db.db_connection import get_db
 from auth.auth_bearer import BearerToken
 from dependencies import get_current_user
+from services.user import UserService
 from schemas.user import AppUser
 
 router = APIRouter(
@@ -20,3 +22,8 @@ router = APIRouter(
 @router.get("/me", response_model=AppUser)
 async def get_current_user_profile(current_user=Depends(get_current_user), db: Session = Depends(get_db)):
     return current_user
+
+
+@router.get("/", response_model=List[AppUser])
+async def get_users(current_user=Depends(get_current_user), db: Session = Depends(get_db), active: bool = True):
+    return UserService(db).get_users(active=active)
