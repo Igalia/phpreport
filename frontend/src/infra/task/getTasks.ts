bd862a2ef696ac4e57bcdb2b9553ef7adbae3724
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-24 12:20:05 -0300

    Add limit param to getTasks

--- frontend/src/infra/task/getTasks.ts
+++ frontend/src/infra/task/getTasks.ts
@@ -1,16 +1,21 @@
 import { ApiClient } from '@/infra/lib/apiClient'
 import { Task } from '@/domain/Task'
 
-type GetTasksProps = Pick<Task, 'userId' | 'startTime' | 'endTime'>
+type GetTasksProps = Pick<Task, 'userId' | 'startTime' | 'endTime'> & { limit?: number }
 
 export const makeGetTasks =
   (apiClient: ApiClient) =>
-  async ({ userId, startTime, endTime }: GetTasksProps): Promise<Array<Task>> => {
+  async ({ userId, startTime, endTime, limit }: GetTasksProps): Promise<Array<Task>> => {
     const params = new URLSearchParams({
       user_id: userId.toString(),
       start: startTime,
       end: endTime
     })
+
+    if (limit) {
+      params.append('limit', limit.toString())
+    }
+
     const response = await apiClient(`/v1/timelog/tasks?${params}`, { next: { tags: ['tasks'] } })
 
     if (!response.ok) {
