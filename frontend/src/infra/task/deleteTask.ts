d341758d2767554729f76d3fa092bfb754dff19e
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-26 12:37:11 -0300

    Move deleteTask to factory pattern

--- frontend/src/infra/task/deleteTask.ts
+++ frontend/src/infra/task/deleteTask.ts
@@ -1,17 +1,20 @@
 import { ApiClient } from '../lib/apiClient'
+import { Task } from '@/domain/Task'
 
-export const deleteTask = async (taskId: number, apiClient: ApiClient): Promise<string> => {
-  return apiClient(`/v1/timelog/tasks/${taskId}`, {
-    method: 'DELETE'
-  })
-    .then((response) => {
+export const makeDeleteTask =
+  (apiClient: ApiClient) =>
+  async (taskId: Task['id']): Promise<string> => {
+    try {
+      const response = await apiClient(`/v1/timelog/tasks/${taskId}`, {
+        method: 'DELETE'
+      })
+
       if (!response.ok) {
         throw Error(response.statusText)
       }
 
       return response.statusText
-    })
-    .catch((e) => {
-      throw new Error(e)
-    })
-}
+    } catch (e) {
+      throw e
+    }
+  }
