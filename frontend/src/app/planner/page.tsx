8a686e15dbb125de19c982ba2e444dc47b8dfa61
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-26 12:36:40 -0300

    Move getTaskTypes, getProjects and getTemplates to factory pattern

--- frontend/src/app/planner/page.tsx
+++ frontend/src/app/planner/page.tsx
@@ -1,11 +1,12 @@
-import { getProjects } from '@/infra/project/getProjects'
+import { makeGetProjects } from '@/infra/project/getProjects'
 import { serverFetch } from '@/infra/lib/serverFetch'
 import { ProjectSelection } from './components/ProjectSelection'
 
 const getPageData = async () => {
   const apiClient = await serverFetch()
+  const getProjects = makeGetProjects(apiClient)
 
-  return getProjects(apiClient)
+  return getProjects()
 }
 
 export default async function ProjectManagement() {
--- frontend/src/infra/taskType/getTaskTypes.ts
+++ frontend/src/infra/taskType/getTaskTypes.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from '@/infra/lib/apiClient'
 import { TaskType } from '@/domain/TaskType'
 
-export const getTaskTypes = async (apiClient: ApiClient): Promise<Array<TaskType>> => {
+export const makeGetTaskTypes = (apiClient: ApiClient) => async (): Promise<Array<TaskType>> => {
   const response = await apiClient('/v1/timelog/task_types/')
 
   if (!response.ok) {
