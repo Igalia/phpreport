5443e6d0a900ad23a143bbb3e4ed625168a14050
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2024-01-26 12:37:11 -0300

    Move deleteTask to factory pattern

--- frontend/src/app/tasks/hooks/useDeleteTask.ts
+++ frontend/src/app/tasks/hooks/useDeleteTask.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Task } from '@/domain/Task'
 import { useAlert } from '@/ui/Alert/useAlert'
-import { deleteTask } from '@/infra/task/deleteTask'
+import { makeDeleteTask } from '@/infra/task/deleteTask'
 import { useClientFetch } from '@/infra/lib/useClientFetch'
 import { useGetCurrentUser } from '@/hooks/useGetCurrentUser/useGetCurrentUser'
 
@@ -10,8 +10,9 @@ export const useDeleteTask = () => {
   const queryClient = useQueryClient()
   const { id: userId } = useGetCurrentUser()
   const { showError, showSuccess } = useAlert()
+  const deleteTask = makeDeleteTask(apiClient)
 
-  const { mutate } = useMutation((taskId: number) => deleteTask(taskId, apiClient), {
+  const { mutate } = useMutation((taskId: number) => deleteTask(taskId), {
     onSuccess: (_, taskId) => {
       queryClient.setQueryData<Array<Task>>(['tasks', userId], (prevData) =>
         prevData!.filter((prevData) => prevData.id !== taskId)
